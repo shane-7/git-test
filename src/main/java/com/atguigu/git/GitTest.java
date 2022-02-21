577585fbde0e4cfbb905e72723ea2e682262a21c
Shane <Shane@ZjLab.com>
2022-02-21 17:21:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-02-21 17:17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
